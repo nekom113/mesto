0078ca084777e33fe7091db0d5fa6d4209c01d27
Alex Piontek <nekom113@mail.ru>
2022-11-29 22:04:18 +0300

    fix index and js file

--- script/index.js
+++ script/index.js
@@ -1,63 +1,39 @@
 const popup = document.querySelector('.popup')
-// const buttonPopupEdit = document.querySelectorAll('[data-popup-open]')
 const editButton = document.querySelector('.profile__edit-button')
-// const buttonPopupSave = document.querySelectorAll('[data-popup-close]')
 const buttonPopupSave = document.querySelector('.popup__close-icon')
-
-const conteinerPopup = document.querySelector('.popup__container')
 const profileName = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const inputEdit = document.querySelectorAll('.popup__input')
+const inputName = document.querySelector('.popup__input_type-name')
+const inputDescription = document.querySelector('.popup__input_type-description')
 const saveEditButton = document.querySelector('.popup__save-button')
-const formElement = document.querySelector('.popup__form')
 
+function getProfileDataToForm(){
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileSubtitle.textContent;
+};
 
+function editProfile(){
+  profileName.textContent = inputName.value;
+  profileSubtitle.textContent = inputDescription.value;
+};
 
 function popupOpen() {
-  popup.classList.add('popup_opened')
-}
+  popup.classList.add('popup_opened');
+  getProfileDataToForm();
+};
+
 function popupClose() {
-  popup.classList.remove('popup_opened')
-}
+  popup.classList.remove('popup_opened');
+  getProfileDataToForm();
+};
 
-// buttonPopupEdit.forEach(el => {
-//   el.addEventListener('click', popupOpen)
-// })
+editButton.addEventListener('click', popupOpen);
 
-// buttonPopupSave.forEach(el => {
-//   el.addEventListener('click', popupClose)
-// })
+buttonPopupSave.addEventListener('click', popupClose);
 
-editButton.addEventListener('click', popupOpen)
-buttonPopupSave.addEventListener('click', () => {
+saveEditButton.addEventListener('click', (e) =>{
+  e.preventDefault();
+  editProfile();
   popupClose();
+});
 
-})
-
-function listenInput() {
-  inputEdit.forEach(el => {
-    console.log(el)
-    if (el.id === 'name') {
-      el.placeholder = profileName.innerText
-    } else if (el.id === 'description') {
-      el.placeholder = profileSubtitle.innerText
-    }
-  })
-}
-
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-
-  
-  .forEach(el => {
-    if (el.id === 'name') {
-      profileName.innerText = el.value
-    } else if (el.id === 'description') {
-      profileSubtitle.innerText = el.value
-
-    }
-  })
-}
-
-listenInput()
-formElement.addEventListener('submit', handleFormSubmit);
